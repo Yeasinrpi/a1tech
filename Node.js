5a65c27d8d7b266f3bb21deef4be646e2b458899
Yeasinrpi <yeasinrpi@gmail.com>
2025-09-16 17:30:05 +0600

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,1 +1,12 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$server = $_GET['server'];
+$conn = @ftp_connect($server, 21, 10);
+if ($conn) {
+    echo "✅ FTP server $server is reachable.";
+    ftp_close($conn);
+} else {
+    echo "❌ Unable to connect to FTP server $server.";
+}
+?>
 
